374808d08070005bff1d9b9d7ebb3641d190b293
mblanke70 <blanke@ursulaschule.de>
2017-10-29 11:02:10 +0100

    first commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+
+}
